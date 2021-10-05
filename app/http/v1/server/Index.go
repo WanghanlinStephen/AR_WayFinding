a3a107f6d29adc5787664e10e6f5dad7d2f551ee
Stephen <u3560396@connect.hku.hk>
2021-10-05 09:38:59 +0800

    initial framework set-up

---
+++ app/http/v1/server/Index.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"pro/app/cache"
+	"pro/app/common/response"
+)
+
+func Index(c *gin.Context) {
+	redis := cache.RedisInter.Get()
+	r, err := redis.Do("Set", "test", 111)
+	if err != nil {
+		fmt.Println(err)
+	}
+	response.Success(c, "ok", r)
+}
+func Search(c *gin.Context) {
+
+
+}
+func Test(c *gin.Context) {
+	type params struct {
+		Id   int    `json:"id"   binding:"required"`
+		Name string `json:"name" binding:"required"`
+	}
+	if err := c.ShouldBind(&params{}); err != nil {
+		fmt.Println(err.Error())
+		response.Error(c, "参数错误")
+	}
+	fmt.Println(params{})
+	response.Success(c,"ok","")
+	//dec := json.NewDecoder(strings.NewReader(jsonstring))
+	//fmt.Println(dec)
+}
---
+++ app/router/route.go
@@ -0,0 +1,42 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+	"pro/app/http/v1/server"
+	"pro/app/middle"
+	"pro/app/socket"
+	"pro/config"
+)
+
+func router(route *gin.Engine) *gin.Engine {
+
+	//socket服务器
+	route.GET("/ws", socket.Run)
+	//route.GET("/ws/ping", socket.Ping)
+
+	v1 := route.Group("/v1")
+	//遊客操作，无需登录
+	visitorAPI := v1.Group("/api")
+	{
+		visitorAPI.GET("index", server.Index)
+		visitorAPI.GET("test", server.Test)
+		visitorAPI.GET("search",server.Search)
+	}
+
+	return route
+}
+
+func RouteInit() *gin.Engine {
+	if config.Mode != "dev" {
+		gin.SetMode(gin.ReleaseMode)
+		gin.DisableConsoleColor()
+	}
+
+	route := gin.New()
+	if config.Mode == "dev" {
+		route.Use(gin.Logger())
+	}
+	route.Use(gin.Recovery()) // 捕捉异常
+	route.Use(middle.Access)
+	return router(route)
+}
